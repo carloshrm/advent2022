e4b9f1099b274a777c4fefa08c2d5ecc80edc965
Carlos Moraes <carloshrmoraes@gmail.com>
2022-12-02 19:37:04 -0300

    file fixes and renamed variables

---
+++ Day2.cs
@@ -0,0 +1,53 @@
+﻿namespace Namespace
+{
+    internal class Day2 : Solution
+    {
+        public Day2() : base(2) { }
+
+        protected override int partOne()
+        {
+            int score = 0;
+            foreach (var match in input.data)
+            {
+                int elfHand = match[0] - '@';
+                int myHand = match[2] - 'W';
+
+                score += myHand;
+                if (myHand == elfHand)
+                    score += 3;
+                else
+                {
+                    if ((myHand == 1 && elfHand == 3) ||
+                        (myHand == 2 && elfHand == 1) ||
+                        (myHand == 3 && elfHand == 2))
+                    {
+                        score += 6;
+                    }
+                }
+            }
+            return score;
+        }
+
+        protected override int partTwo()
+        {
+            int[] matchingWin = { 2, 3, 1 };
+            int[] matchingLoss = { 3, 1, 2 };
+            int score = 0;
+            foreach (var match in input.data)
+            {
+                int elfHand = match[0] - '@';
+                int outcome = match[2] - 'W';
+
+                score += (outcome - 1) * 3;
+
+                if (outcome == 3)
+                    score += matchingWin[elfHand - 1];
+                else if (outcome == 1)
+                    score += matchingLoss[elfHand - 1];
+                else
+                    score += elfHand;
+            }
+            return score;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,61 +8,7 @@
             //solutions.Add(new Day1());
             solutions.Add(new Day2());
 
-            foreach (var s in solutions)
-            {
-                s.runSolution();
-            }
-        }
-    }
-
-    internal class Day2 : Solution
-    {
-        public Day2() : base(2) { }
-
-        protected override int partOne()
-        {
-            int score = 0;
-            foreach (var o in input.data)
-            {
-                int elfHand = o[0] - '@';
-                int myHand = o[2] - 'W';
-
-                score += myHand;
-                if (myHand == elfHand)
-                    score += 3;
-                else
-                {
-                    if ((myHand == 1 && elfHand == 3) ||
-                        (myHand == 2 && elfHand == 1) ||
-                        (myHand == 3 && elfHand == 2))
-                    {
-                        score += 6;
-                    }
-                }
-            }
-            return score;
-        }
-
-        protected override int partTwo()
-        {
-            int[] matchingWin = { 2, 3, 1 };
-            int[] matchingLoss = { 3, 1, 2 };
-            int score = 0;
-            foreach (var o in input.data)
-            {
-                int elfHand = o[0] - '@';
-                int outcome = o[2] - 'W';
-
-                score += (outcome - 1) * 3;
-
-                if (outcome == 3)
-                    score += matchingWin[elfHand - 1];
-                else if (outcome == 1)
-                    score += matchingLoss[elfHand - 1];
-                else
-                    score += elfHand;
-            }
-            return score;
+            solutions.ForEach(x => x.runSolution());
         }
     }
 }
