71846b46e5015494fec4daf2933c4fe5f5f709c5
Carlos Moraes <carloshrmoraes@gmail.com>
2022-12-07 14:07:23 -0300

    elf refactor

---
+++ elves/FileElf.cs
@@ -0,0 +1,56 @@
+﻿namespace Namespace
+{
+    internal class FileElf
+    {
+        private readonly string path = "data/day{0}{1}.txt";
+        private static FileElf? _elf;
+
+        private FileElf() { }
+
+        public static FileElf callElf()
+        {
+            if (_elf == null)
+                _elf = new FileElf();
+
+            return _elf;
+        }
+
+        public string[] getInput(int day)
+        {
+            var mainData = getDataFromFile(day, isExample: false);
+            if (mainData.Any() is false)
+            {
+                var request = HttpElf.callElf().getDataFromHttp(day);
+                mainData = request.Result.Split("\n");
+                File.WriteAllLines(string.Format(path, day, ""), mainData);
+            }
+            return mainData;
+        }
+
+        public string[] getExample(int day)
+        {
+            var exampleData = getDataFromFile(day, isExample: true); ;
+            if (exampleData.Any() is false)
+            {
+                var request = HttpElf.callElf().getExampleFromHttp(day);
+                exampleData = request.Result.Split("\n");
+                File.WriteAllLines(string.Format(path, day, "e"), exampleData);
+            }
+            return exampleData;
+        }
+
+        private string[] getDataFromFile(int day, bool isExample)
+        {
+            try
+            {
+                return File.ReadAllLines(string.Format(path, day, isExample ? "e" : ""));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return new string[0];
+            }
+        }
+    }
+
+}
---
+++ elves/HttpElf.cs
@@ -0,0 +1,63 @@
+﻿using System.Net;
+using System.Net.Http.Headers;
+
+namespace Namespace
+{
+    internal class HttpElf
+    {
+        private static HttpClient? _httpClient;
+        private readonly string siteURL = "https://adventofcode.com/";
+        private static HttpElf? _elf;
+
+        private HttpElf()
+        {
+            var httpClientHandler = new HttpClientHandler();
+            _httpClient = new HttpClient(httpClientHandler);
+            _httpClient.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("(+https://github.com/carloshrm/advent2022)"));
+            _httpClient.BaseAddress = new Uri(siteURL);
+
+            var cookieContainer = new CookieContainer();
+            cookieContainer.Add(_httpClient.BaseAddress, new Cookie("session", Environment.GetEnvironmentVariable("session")));
+            httpClientHandler.CookieContainer = cookieContainer;
+        }
+
+        public static HttpElf callElf()
+        {
+            if (_elf == null)
+                _elf = new HttpElf();
+
+            return _elf;
+        }
+
+        public async Task<string> getDataFromHttp(int day)
+        {
+            var resp = _httpClient!.SendAsync(new HttpRequestMessage(HttpMethod.Get, $"/2022/day/{day}/input"));
+            Console.WriteLine("http call");
+            return await resp.Result.Content.ReadAsStringAsync();
+        }
+
+        public async Task<string> getExampleFromHttp(int day)
+        {
+            var resp = _httpClient!.SendAsync(new HttpRequestMessage(HttpMethod.Get, $"/2022/day/{day}"));
+            var rawHtml = await resp.Result.Content.ReadAsStringAsync();
+            Console.WriteLine("http call");
+            return findExample(rawHtml);
+        }
+
+        private string findExample(string rawHtml)
+        {
+            var result = string.Empty;
+            int start = 0;
+            do
+            {
+                var startingIndex = rawHtml.IndexOf("<pre><code>", start) + "<pre><code>".Length;
+                var endingIndex = rawHtml.IndexOf("</code>", startingIndex);
+                result = rawHtml.Substring(startingIndex, endingIndex - startingIndex);
+                start = endingIndex;
+            } while (result.Contains("<"));
+
+            return result;
+        }
+    }
+
+}
