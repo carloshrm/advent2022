fb197135cf53e5a01d9d3b4827a07f33a70573d0
Carlos Moraes <carloshrmoraes@gmail.com>
2022-12-10 10:18:06 -0300

    dau8 solution

--- Solutions/Day8.cs
+++ Solutions/Day8.cs
@@ -1,19 +1,12 @@
-﻿namespace Namespace
+﻿namespace Solutions
 {
     internal class Day8 : Solution<int>
     {
-        private ForestTree[][] _forest;
+        private ForestTree[][] _forest { get; init; }
 
-        public Day8() : base(8)
-        {
-            //input.data = input.example;
-            _forest = setTrees();
-        }
+        public Day8() : base(8) => _forest = setTrees();
 
-        private bool isEdge(int x, int y)
-        {
-            return x == 0 || y == 0 || x == input.data.Length - 1 || y == input.data.First().Length - 1;
-        }
+        private bool isEdge(int x, int y) => x == 0 || y == 0 || x == input.data.Length - 1 || y == input.data.First().Length - 1;
 
         protected override int partOne()
         {
@@ -22,12 +15,16 @@
             {
                 for (int j = 1; j < _forest[i].Length - 1; j++)
                 {
-                    if (checkVisibility(i, j, 0, 1) ||
-                        checkVisibility(i, j, 1, 0) ||
-                        checkVisibility(i, j, -1, 0) ||
-                        checkVisibility(i, j, 0, -1))
+                    (bool up, bool right, bool left, bool down)
+                        visibility = (
+                        checkVisibility(i, j, 0, 1),
+                        checkVisibility(i, j, 1, 0),
+                        checkVisibility(i, j, -1, 0),
+                        checkVisibility(i, j, 0, -1)
+                        );
+
+                    if (visibility.up || visibility.left || visibility.right || visibility.down)
                     {
-                        _forest[i][j].isVisible = true;
                         totalVisible++;
                     }
                 }
@@ -35,26 +32,30 @@
             return totalVisible;
         }
 
-        protected override int partTwo()
-        {
-            return base.partTwo();
-        }
+        protected override int partTwo() => _forest.Max(f => f.Max(tr => tr.scenicScore));
 
         private bool checkVisibility(int i, int j, int iX, int jX)
         {
+            int s = 0;
             while ((i + iX) < _forest.Length && i + iX >= 0 && j + jX < _forest[i].Length && j + jX >= 0)
             {
+                s++;
                 if (_forest[i][j].height <= _forest[i + iX][j + jX].height)
+                {
+                    _forest[i][j].scenicScore *= s;
                     return false;
-                iX += iX == 0 ? 0 : (iX > 0 ? 1 : -1);
-                jX += jX == 0 ? 0 : (jX > 0 ? 1 : -1);
+                }
+                if (iX != 0) iX += iX > 0 ? 1 : -1;
+                if (jX != 0) jX += jX > 0 ? 1 : -1;
             }
+            _forest[i][j].scenicScore *= s;
+            _forest[i][j].isVisible = true;
             return true;
         }
 
         private ForestTree[][] setTrees()
         {
-            var newForest = new ForestTree[input.data.Length][].Select(_ => new ForestTree[input.data.First().Length]).ToArray();
+            var newForest = new ForestTree[input.data.Length][].Select(_ => new ForestTree[input.data[0].Length]).ToArray();
             for (int i = 0; i < input.data.Length; i++)
             {
                 for (int j = 0; j < input.data[i].Length; j++)
@@ -71,13 +72,14 @@
         {
             public bool isVisible { get; set; }
             public int height { get; init; }
+            public int scenicScore { get; set; }
 
             public ForestTree(char val)
             {
                 isVisible = false;
-                this.height = (int)char.GetNumericValue(val);
+                height = (int)char.GetNumericValue(val);
+                scenicScore = 1;
             }
-
         }
     }
 }
