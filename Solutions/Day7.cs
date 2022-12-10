d9ae4dabf187d3c8e081af4de520f8b6afd1aac7
Carlos Moraes <carloshrmoraes@gmail.com>
2022-12-10 11:47:26 -0300

    day7 refactor

--- Solutions/Day7.cs
+++ Solutions/Day7.cs
@@ -2,44 +2,110 @@
 {
     internal class Day7 : Solution<int>
     {
-        private ElfFolder _fileSystem { get; init; }
-
+        private readonly ElfFsManager _fsManager;
         public Day7() : base(7)
         {
-            _fileSystem = buildDirectoryTree();
-            setTotalSizes(_fileSystem);
+            _fsManager = ElfFsManager.getManager();
+            _fsManager.buildDirectoryTree(input.data);
+            _fsManager.setTotalSizes(_fsManager.getRoot());
         }
 
         protected override int partOne()
         {
             var foldersInRange = new List<ElfFolder>();
-            selectByRange(100000, _fileSystem, foldersInRange);
+            _fsManager.selectByRange(100000, foldersInRange, _fsManager.getRoot());
             return foldersInRange.Sum(z => z.totalSize);
         }
 
         protected override int partTwo()
         {
             var withinLimit = new List<ElfFolder>();
-            selectDeletionCandidates(70000000 - _fileSystem.totalSize, _fileSystem, withinLimit);
+            _fsManager.selectDeletionCandidates(freeSpace: 70000000 - _fsManager.getRoot().totalSize, withinLimit, _fsManager.getRoot());
             return withinLimit.Min(fdr => fdr.totalSize);
         }
+    }
 
-        private ElfFolder buildDirectoryTree()
+    internal sealed class ElfFile
+    {
+        public int size;
+        public string name;
+
+        public ElfFile(int size, string name)
         {
-            var root = new ElfFolder(null, "/");
-            var currentFolder = root;
+            this.size = size;
+            this.name = name;
+        }
+    }
 
-            for (int i = 0; i < input.data.Length; i++)
+    internal sealed class ElfFolder
+    {
+        private readonly string name;
+        private readonly List<ElfFile> files;
+
+        public int totalSize { get; set; }
+        public ElfFolder? parent { get; init; }
+        public List<ElfFolder> folders { get; set; }
+
+        public ElfFolder(ElfFolder? parent, string name)
+        {
+            folders = new List<ElfFolder>();
+            files = new List<ElfFile>();
+            this.parent = parent;
+            this.name = name;
+        }
+
+        public void touch(string name, int size)
+        {
+            files.Add(new ElfFile(size, name));
+            totalSize += size;
+        }
+
+        public void mkdir(string name) => folders.Add(new ElfFolder(this, name));
+
+        public ElfFolder cd(string name) => folders.First(f => f.name == name);
+    }
+
+    internal sealed class ElfFsManager
+    {
+        private static ElfFsManager _folderManager;
+        private ElfFolder? _root;
+
+        private ElfFsManager() { }
+
+        public static ElfFsManager getManager()
+        {
+            if (_folderManager == null)
+                _folderManager = new ElfFsManager();
+
+            return _folderManager;
+        }
+
+        public ElfFolder? getRoot() => _root;
+
+        public int setTotalSizes(ElfFolder currentFolder)
+        {
+            foreach (var dir in currentFolder.folders)
+                currentFolder.totalSize += setTotalSizes(dir);
+
+            return currentFolder.totalSize;
+        }
+
+        public void buildDirectoryTree(string[] source)
+        {
+            var newRoot = new ElfFolder(null, "/");
+            var currentFolder = newRoot;
+
+            for (int i = 0; i < source.Length; i++)
             {
-                var commandSyntax = input.data[i].Split(' ');
+                var commandSyntax = source[i].Split(' ');
                 if (commandSyntax[0] == "$")
                 {
                     if (commandSyntax[1] == "cd")
                     {
                         if (commandSyntax[2] == "..")
-                            currentFolder = currentFolder.parent;
+                            currentFolder = currentFolder!.parent;
                         else if (commandSyntax[2] == "/")
-                            currentFolder = root;
+                            currentFolder = newRoot;
                         else
                             currentFolder = currentFolder.cd(commandSyntax[2]);
                     }
@@ -52,76 +118,25 @@
                         currentFolder.touch(commandSyntax[1], int.Parse(commandSyntax[0]));
                 }
             }
-            return root;
+            _root = newRoot;
         }
-
-        private void selectByRange(int limit, ElfFolder currentFolder, List<ElfFolder> selection)
+        public void selectByRange(int limit, List<ElfFolder> selection, ElfFolder currentFolder)
         {
             foreach (var dir in currentFolder.folders)
             {
-                selectByRange(limit, dir, selection);
+                selectByRange(limit, selection, dir);
                 if (dir.totalSize < limit)
                     selection.Add(dir);
             }
         }
 
-        private int setTotalSizes(ElfFolder currentFolder)
+        public void selectDeletionCandidates(int freeSpace, List<ElfFolder> deletable, ElfFolder currentFolder)
         {
             foreach (var dir in currentFolder.folders)
-                currentFolder.totalSize += setTotalSizes(dir);
-            return currentFolder.totalSize;
-        }
+                selectDeletionCandidates(freeSpace, deletable, dir);
 
-        private void selectDeletionCandidates(int freeSpace, ElfFolder currentFolder, List<ElfFolder> deletable)
-        {
-            foreach (var dir in currentFolder.folders)
-                selectDeletionCandidates(freeSpace, dir, deletable);
             if (freeSpace + currentFolder.totalSize >= 30000000)
                 deletable.Add(currentFolder);
-        }
-        private sealed class ElfFile
-        {
-            public int size;
-            public string name;
-
-            public ElfFile(int size, string name)
-            {
-                this.size = size;
-                this.name = name;
-            }
-        }
-
-        private sealed class ElfFolder
-        {
-            public string name;
-            public ElfFolder? parent;
-            public int totalSize;
-            public List<ElfFolder> folders { get; set; }
-            public List<ElfFile> files { get; set; }
-
-            public ElfFolder(ElfFolder? parent, string name)
-            {
-                folders = new List<ElfFolder>();
-                files = new List<ElfFile>();
-                this.parent = parent;
-                this.name = name;
-            }
-
-            public void touch(string name, int size)
-            {
-                files.Add(new ElfFile(size, name));
-                totalSize += size;
-            }
-
-            public void mkdir(string name)
-            {
-                folders.Add(new ElfFolder(this, name));
-            }
-
-            public ElfFolder cd(string name)
-            {
-                return folders.First(f => f.name == name);
-            }
         }
     }
 }
