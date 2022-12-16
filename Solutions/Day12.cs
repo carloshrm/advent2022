2cf4adacb1e42e3df240876eafd361280ef623d1
Carlos Moraes <carloshrmoraes@gmail.com>
2022-12-16 14:16:03 -0300

    day12 p2

--- Solutions/Day12.cs
+++ Solutions/Day12.cs
@@ -19,38 +19,46 @@
     internal class Day12 : Solution<int, int>
     {
         private ElfNode<char>[][] _map;
+
         public Day12() : base(12)
         {
             _map = input.data.Select((l, i) => l.Select((c, j) => new ElfNode<char>(c, i, j)).ToArray()).ToArray();
         }
 
-        protected override int partOne()
-        {
-            var explorationDue = new Queue<ElfNode<char>>();
+        protected override int partOne() => traverseMap('S', 'E');
 
-            var startingPosition = _map.First(r => r.Any(y => y.height == 'S')).First();
-            startingPosition.height = 'a';
+        protected override int partTwo() => traverseMap('E', 'a');
+
+        private int traverseMap(char from, char to)
+        {
+            resetMap();
+            var explorationDue = new Queue<ElfNode<char>>();
+            var startingPosition = _map.SelectMany(r => r).First(y => y.height == from);
+            from = from == 'S' ? 'a' : 'z';
+            startingPosition.height = from;
             explorationDue.Enqueue(startingPosition);
 
-            while (explorationDue.Any())
+            while (explorationDue.Count != 0)
             {
                 var current = explorationDue.Dequeue();
-
                 if (current.wasVisited)
                     continue;
                 current.wasVisited = true;
 
-                char height = current.height;
-
                 foreach (var direction in new (int r, int c)[] { (-1, 0), (1, 0), (0, -1), (0, 1) })
                 {
                     (int nr, int nc) = (current.row + direction.r, current.col + direction.c);
+
                     if (nr >= 0 && nr < _map.Length && nc >= 0 && nc < _map[nr].Length)
                     {
-                        if (_map[nr][nc].height == 'E' && height == 'z')
+                        (var nxt, var crr) = (_map[nr][nc].height, current.height);
+
+                        if (nxt == to && crr == (to == 'E' ? 'z' : to + 1))
                             return current.step + 1;
 
-                        if (current.height - _map[nr][nc].height >= -1)
+                        if (from > (to == 'E' ? 'z' : to + 1))
+                            (nxt, crr) = (crr, nxt);
+                        if (nxt - crr <= 1)
                         {
                             _map[nr][nc].step = current.step + 1;
                             explorationDue.Enqueue(_map[nr][nc]);
@@ -59,10 +67,19 @@
                 }
 
             }
-            return 0;
+            return -1;
         }
-
-        protected override int partTwo() => 0;
+        private void resetMap()
+        {
+            foreach (var ln in _map)
+            {
+                foreach (var n in ln)
+                {
+                    n.step = 0;
+                    n.wasVisited = false;
+                }
+            }
+        }
 
     }
 }
